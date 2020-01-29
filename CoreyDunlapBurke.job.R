fafed3591411265615c87aae9bcea0af2c25bf6a
SigurdJanson <emailjs@web.de>
2020-01-29 23:50:28 +0100

    Complete refactoring, speed is 50% better

--- CoreyDunlapBurke.job.R
+++ CoreyDunlapBurke.job.R
@@ -13,50 +13,103 @@ M <- c("Default", "Fisher")
 NConditions <- length(D) * length(N) * length(R)
 NIterations <- 1000
 
-Result <- array(0, dim = c(length(D), length(N), length(R), length(M)),
-                dimnames = list(paste0("D", D), paste0("N", N), 
-                                paste0("Rho=", R), M))
-#Result <- provideDimnames(Result, sep="", base = list(c("D", "SampleSize", "Rho", "Method")))
-#, sep = "_", base = list('row','col','lev')
-for(d in D) {
-  cat("D = ", d, "\n")
-  for(n in N) {
-    for(r in R) {
-      for(i in 1:NIterations) {
-        # Generate correlated data
-        SimDat <- genCorData(n, mu = rep(0, d), sigma = rep(1, d), rho = r, corstr = "cs")
-        # Estimate correlation matrix
-        CorMat <- cor(SimDat[, 2:(d+1)])
-        # Average it - use only lower triangle of corr. matrix
-        Correls <- CorMat[lower.tri(CorMat)]
-        MeanD <- MeanR( Correls, n, Method = "Default")
-        MeanF <- MeanR( Correls, n, Method = "Fisher")
-        #MeanH <- MeanR( CorMat, n, Method = "Hotelling")
-        
-        # Sum up
-        # IdxD <- which(D == d)
-        # IdxN <- which(N == n)
-        # IdxR <- which(R == r)
-        # Result[IdxD, IdxN, IdxR, 1] <- Result[IdxD, IdxN, IdxR, 1] + MeanD
-        # Result[IdxD, IdxN, IdxR, 2] <- Result[IdxD, IdxN, IdxR, 2] + MeanF
-        #Result[IdxD, IdxN, IdxR, 3] <- Result[IdxD, IdxN, IdxR, 3] + MeanH
-      }
-    }
+
+# Create data frame
+# EmptyDf <- data.frame(Rho       = factor(numeric(), levels = R),
+#                  SampleSize = factor(numeric(), levels = N),
+#                  Samples    = factor(numeric(), levels = D),
+#                  Method     = factor(character(), levels = M),
+#                  Iteration  = factor(numeric(), levels = 1:NIterations),
+#                  RObs       = numeric(),
+#                  RDelta     = numeric(),
+#                  stringsAsFactors = FALSE)
+
+
+#' @note This function creates its own list for each piece of data.
+#' Afterwards this list of lists (each with equal length) will be
+#' coerced into a data frame.
+RandomSample <- function( Iteration, SampleSize, Rho ) {
+  Names <- c("Rho", "SampleSize", "Samples", "Method", "Iteration", 
+             "RObs", "RDelta")
+  # Data
+  d <- max(D) # global variable D 
+  # Generate correlated data - one sample for all sample sizes
+  SimDat <- genCorData(SampleSize, mu = rep(0, d), sigma = rep(1, d), rho = Rho, corstr = "cs")
+  # Estimate correlation matrix
+  CorMat <- cor(SimDat[, 2:(d+1)])
+  
+  Result <- vector(mode = "list", length = length(Names))
+  names(Result) <- Names
+  for(Samples in D) {
+    # Choose subset of the random sample
+    SubSample <- sample(1:d, Samples)
+
+    # Use only lower triangle of corr. matrix
+    PartialCorMat <- CorMat[SubSample, SubSample]
+    Correls <- PartialCorMat[lower.tri(PartialCorMat)]
+
+    # Add data to lists - append some twice for there are two method values
+    Result$Rho  <- c(Result$Rho, rep(Rho, 2)) # 
+    Result$SampleSize  <- c(Result$SampleSize, rep(SampleSize, 2)) # 
+    Result$Samples  <- c(Result$Samples, rep(Samples, 2)) # 
+    Result$Iteration  <- c(Result$Iteration, rep(Iteration, 2)) # 
+
+    RObs <- MeanR( Correls, SampleSize, Method = "Fisher")
+    Result$Method  <- c(Result$Method, "Default") 
+    Result$RObs    <- c(Result$RObs, RObs)
+    Result$RDelta  <- c(Result$RDelta, Rho - RObs)
+    
+    RObs <- MeanR( Correls, SampleSize, Method = "Fisher")
+    Result$Method  <- c(Result$Method, "Fisher") 
+    Result$RObs    <- c(Result$RObs, RObs)
+    Result$RDelta  <- c(Result$RDelta, Rho - RObs)
+    
+    #RObs <- MeanR( CorMat, n, Method = "Hotelling")
+    #...
+  }
+  
+  class(Result) <- "data.frame"
+  attr(Result, "row.names") <- .set_row_names(length(Result[[1]]))
+  return(Result)
+}
+
+TotalList <- vector("list", length(N)*length(R)) #list()
+list_of_dfs <- list()
+for(n in N) {
+  cat(n, "\n")
+  for(r in R) {
+      list_of_dfs <- lapply(1:NIterations, RandomSample, SampleSize = n, Rho = r)
+      TotalList <- append(TotalList, list_of_dfs)
   }
 }
-Result <- Result / NIterations
-#summary(Result)
+cat("Almost done\n")
 
-AddLine <- function(x, y, NameD, NameN...) {
-  #hasName(y)
-  NameD <- "" # ignore it - avoid warning
-  Color <- c("#023FA5","#A1A6C8","#E2E2E2","#CA9CA4","#8E063B")
-  Color <- Color[which(paste0("N", N) == NameN)]
-  lines(x, y, col = Color, ...)
-}
+rm(list_of_dfs)
+df <- Reduce(rbind, TotalList)
+rm(TotalList)
 
-plot(R, R - R, ylim = c(-0.1, 0.1), pch=".")
-#RDiff <- apply(Result[,,, "Default"], c(1, 2), function(x){R-x})
-RDiff <- apply(Result, c(1, 2, 4), function(x){R-x})
-apply(RDiff[,,, "Default"], c(2, 3), AddLine, x=R, NameN = )
-apply(RDiff[,,, "Fisher"], c(2, 3), AddLine, x=R)
+df$Rho <- as.factor(df$Rho)
+df$SampleSize <- as.factor(df$SampleSize)
+df$Samples <- as.factor(df$Samples)
+df$Method <- as.factor(df$Method)
+df$Iteration <- as.factor(df$Iteration)
+
+# Check, if the data is plausible
+# see https://www.littlemissdata.com/blog/simple-eda
+# see https://cran.r-project.org/web/packages/DataExplorer/vignettes/dataexplorer-intro.html
+#summary(df)
+#library(DataExplorer)
+#plot_bar(df)
+
+
+
+# AddLine <- function(x, y, NameD, NameN...) {
+#   #hasName(y)
+#   NameD <- "" # ignore it - avoid warning
+#   Color <- c("#023FA5","#A1A6C8","#E2E2E2","#CA9CA4","#8E063B")
+#   Color <- Color[which(paste0("N", N) == NameN)]
+#   lines(x, y, col = Color, ...)
+# }
+
+
+#plot(R, R - R, ylim = c(-0.1, 0.1), pch=".")
