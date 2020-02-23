24082290ad42c01d420c67195457e7f36a44530e
SigurdJanson <emailjs@web.de>
2020-02-23 22:04:21 +0100

    Added simulation for independent correlations

--- CoreyDunlapBurke.job.R
+++ CoreyDunlapBurke.job.R
@@ -6,23 +6,13 @@ library(simstudy)
 source("./CorrAggBias.R")
 
 
-# Create data frame
-# EmptyDf <- data.frame(Rho   = factor(numeric(), levels = R),
-#                  SampleSize = factor(numeric(), levels = N),
-#                  Samples    = factor(numeric(), levels = D),
-#                  Method     = factor(character(), levels = M),
-#                  Iteration  = factor(numeric(), levels = 1:NIterations),
-#                  RObs       = numeric(),
-#                  RDelta     = numeric(),
-#                  stringsAsFactors = FALSE)
-
-
 # Variables ----
 N <- seq(10, 50, 10)   # sample size
 R <- seq(0.00, 0.95, 0.05)  # Rho
 D <- 3:10              # Data sets to correlate
-#M <- c("None", "Fisher", "Hotelling", "MinVar", "TrueK", "Precise")
-M <- c("TrueK") # Correction method
+#M <- c("None", "Fisher", "Hotelling", "Hotelling2", "MinVar", "TrueK", "Precise", "Squared")
+M <- c("Fisher") # Correction method
+DataStruc <- "Indie" # structure of the data, one of c("Matrix", "Indie", )
 NConditions <- length(D) * length(N) * length(R) * length(M)
 NIterations <- 50000
 
@@ -70,7 +60,8 @@ RandomSample_Matrix <- function(Iteration, SampleSize, Rho) {
     PartialCorMat <- CorMat[SubSample, SubSample]
     Correls <- PartialCorMat[lower.tri(PartialCorMat)]
 
-    for(m in M) {
+    #for(m in M) {
+      m <- M
       Result$Rho[Index]        <<- Rho #
       Result$SampleSize[Index] <<- SampleSize #
       Result$Samples[Index]    <<- Samples  #
@@ -81,7 +72,7 @@ RandomSample_Matrix <- function(Iteration, SampleSize, Rho) {
       Result$RObs[Index]    <<- RObs
       Result$RDelta[Index]  <<- Rho - RObs
       Index <<- Index +1
-    }
+    #}
   }#for
 }
 
@@ -97,60 +88,60 @@ RandomSample_Matrix <- function(Iteration, SampleSize, Rho) {
 #' - D (the number of data sets to be correlated and averaged) {Read}
 #' - Result (where the averaged correlations are stored) {Read/Write}
 RandomSample_Indie <- function(Iteration, SampleSize, Rho) {
+  genIndieData <- function(n, mu, sigma = 1, rho) {
+    k <- length(mu)
+    NValues <- n * k
+    Comm <- rnorm(NValues, mu[1], sigma)
+    ErrX <- rnorm(NValues, mu[1], sigma)
+    ErrY <- rnorm(NValues, mu[1], sigma)
+    X <- Comm * sqrt(rho) + ErrX * sqrt(1-rho)
+    Y <- Comm * sqrt(rho) + ErrY * sqrt(1-rho)
+    X <- split(X, rep(1:k, each = n))
+    Y <- split(Y, rep(1:k, each = n))
+    R <- mapply(cor, X, Y)
+    
+  }
   # Data
   MaxD <- max(D) # global variable D
+  MaxDR <- MaxD * (MaxD-1) / 2
   # Generate correlated data - one sample for all sample sizes
-  SimDat <- genCorData(SampleSize, mu = rep(0, MaxD),
-                       sigma = rep(1, MaxD), rho = Rho, corstr = "cs")
-  # Estimate correlation matrix
-  CorMat <- cor( SimDat[, 2:(MaxD+1)] )
-  
+  AllCorrels <- genIndieData(SampleSize, mu = rep(0, MaxDR), rho = Rho)
+
   for(Samples in D) {
     # Choose subset of the random sample
-    SubSample <- sample(1:MaxD, Samples)
+    SubSample <- sample(1:MaxDR, Samples)
+    Correls   <- AllCorrels[SubSample]
     
-    # Use only lower triangle of corr. matrix
-    PartialCorMat <- CorMat[SubSample, SubSample]
-    Correls <- PartialCorMat[lower.tri(PartialCorMat)]
-    
-    # Add data to lists - append some twice for there are two method values
+    #for(m in M) {
+    m <- M
     Result$Rho[Index]        <<- Rho #
     Result$SampleSize[Index] <<- SampleSize #
     Result$Samples[Index]    <<- Samples  #
     Result$Iteration[Index]  <<- Iteration #
     
-    RObs <- MeanR(Correls, SampleSize, Method = "None")
-    Result$Method[Index]  <<- "None"
+    RObs <- MeanR(Correls, SampleSize, Method = m)
+    Result$Method[Index]  <<- m
     Result$RObs[Index]    <<- RObs
     Result$RDelta[Index]  <<- Rho - RObs
     Index <<- Index +1
-    
-    Result$Rho[Index]        <<- Rho #
-    Result$SampleSize[Index] <<- SampleSize #
-    Result$Samples[Index]    <<- Samples  #
-    Result$Iteration[Index]  <<- Iteration #
-    
-    RObs <- MeanR(Correls, SampleSize, Method = "Fisher")
-    Result$Method[Index]  <<- "Fisher"
-    Result$RObs[Index]    <<- RObs
-    Result$RDelta[Index]  <<- Rho - RObs
-    Index <<- Index +1
-    
-    #RObs <- MeanR( CorMat, n, Method = "Hotelling")
-    #...
+    #}
   }#for
 }
 
 
 
 # Computations ----
+if(DataStruc == "Indie")
+  RandomSample <- RandomSample_Indie
+if(DataStruc == "Matrix")
+  RandomSample <- RandomSample_Matrix
 
 Index <- 1 # Index to iterate through data
-cat(M, "\n")
+cat(M, " - ", DataStruc, "\n")
 for(n in N) {
   cat(n, "\n")
   for(r in R) {
-    lapply(1:NIterations, RandomSample_Matrix, SampleSize = n, Rho = r)
+    lapply(1:NIterations, RandomSample, SampleSize = n, Rho = r)
   }
 }
 class(Result) <- "data.frame"
@@ -166,4 +157,4 @@ rm(Names, Index, n, r)
 
 
 # STORAGE ----
-save(Result, file=paste("./data/CoreyDunlapBurke", M, "Raw.Rda", sep = "_"))
+save(Result, file=paste("./data/CoreyDunlapBurke", DataStruc, M, "Raw.Rda", sep = "_"))
